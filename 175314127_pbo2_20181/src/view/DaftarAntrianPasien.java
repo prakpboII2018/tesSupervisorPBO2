3290d622cf6a98c0d80399f79bb0b11a4ea235d3
admin <admin@9b>
2018-09-05 11:37:11 +0700

    v 0.25

--- 175314127_pbo2_20181/src/view/DaftarAntrianPasien.java
+++ 175314127_pbo2_20181/src/view/DaftarAntrianPasien.java
@@ -15,16 +15,37 @@ import javax.swing.JTextField;
  * @author Alexander Adam
  */
 public class DaftarAntrianPasien extends JDialog{
+
+    
     private JLabel judulLabel;
     private JLabel namaLabel;
     private JTextField namaText;
     private JButton saveButton;
     
+    public DaftarAntrianPasien(String judul){
+        this.setTitle(judul);
+        init();
+        Text();
+    }
     public void init(){
         this.setLayout(null);
         
-        namaLabel = new JLabel("Daftar Pasien");
-        namaLabel.setBounds(100, 200 ,250 ,100);
+        judulLabel = new JLabel("Daftar Antrian Pasien");
+        judulLabel.setBounds(150, 50 ,150 ,50);
         this.add(judulLabel);
+        
+        namaLabel= new JLabel("nama :");
+        namaLabel.setBounds(60,100,100,50);
+        this.add(namaLabel);
+        
+        
+      
+    }
+    public void Text(){
+              
+        namaText = new JTextField();
+        namaText.setBounds(105, 115,50,200);
+        namaText.setSize(50, 20);
+        this.add(namaText);
     }
 }
