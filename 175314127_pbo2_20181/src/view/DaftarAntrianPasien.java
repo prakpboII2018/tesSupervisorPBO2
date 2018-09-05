75f4a59fcb24ae7b1baaec1fb566ab4724a610f3
admin <admin@9b>
2018-09-05 10:49:43 +0700

    v 0.23

---
+++ 175314127_pbo2_20181/src/view/DaftarAntrianPasien.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author admin
+ */
+public class DaftarAntrianPasien extends JDialog{
+    private JLabel judulLabel;
+    private JLabel namaLabel;
+    private JTextField namaText;
+    private JButton saveButton;
+    
+    public void init(){
+        this.setLayout(null);
+        
+        namaLabel = new JLabel("Daftar Pasien");
+    }
+}
